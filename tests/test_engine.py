b2f36b336b618946384341b133c43d69f82a437a
rafaellcoellho <rafaellcoellho@gmail.com>
2020-03-09 21:02:23 -0300

    testa is_valid_selection

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,16 @@
-def test_begin_with_player_one(board):
-    from engine.defines import Players
+from engine.defines import Players
 
+
+def test_begin_with_player_one(board):
     assert board.active_player is Players.P1
+
+
+def test_is_valid_selection(board):
+    assert board.is_valid_selection(0, 0) is True
+    assert board.is_valid_selection(7, 7) is False
+    assert board.is_valid_selection(0, 1) is False
+
+    board.active_player = Players.P2
+    assert board.is_valid_selection(0, 0) is False
+    assert board.is_valid_selection(7, 7) is True
+    assert board.is_valid_selection(0, 1) is False
