e70eb10be2310208e7d3ad8468c2636b89daeabf
rafaellcoellho <rafaellcoellho@gmail.com>
2020-03-15 16:20:12 -0300

    testa se tentou mover para um index invalido

--- tests/engine/test_move.py
+++ tests/engine/test_move.py
@@ -10,3 +10,10 @@ def test_move_to_occupied_square(empty_board, players):
     empty_board.pieces[3][3] = Checker(players[0], *cn("D4"))
     empty_board.pieces[4][4] = Checker(players[0], *cn("E5"))
     assert empty_board.move(*cn("D4", "E5")) is False
+
+
+def test_move_invalid_index(empty_board):
+    empty_board.active_player = Players.P1
+    empty_board.pieces[3][3] = Checker(Players.P1, *cn("D4"))
+    assert empty_board.move(*cn("D4", "A9")) is False
+    assert empty_board.move(*cn("I1", "E5")) is False
