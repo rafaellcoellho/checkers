f3bd3e22201e28381c870b23495aa9ed7b453a9a
rafaellcoellho <rafaellcoellho@gmail.com>
2020-05-16 10:15:36 -0300

    implementação do client do jogo

--- src/network/client.py
+++ src/network/client.py
@@ -3,12 +3,14 @@ import socket
 
 class Client:
     def __init__(self, host, port):
-        self.connection_info = (host, port)
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect_client(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(self.connection_info)
-            s.sendall(b'Hello, world')
-            data = s.recv(1024)
+    def connect(self):
+        connection_info = (self.host, self.port)
+        self.socket.connect(connection_info)
 
-        print('Received', repr(data))
+    def send_to_server(self, data: str):
+        data_in_bytes = str.encode(data)
+        self.socket.send(data_in_bytes)
