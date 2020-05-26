08870889f1e3b4a5f3c64cbe81757a1584967dbf
rafaellcoellho <rafaellcoellho@gmail.com>
2020-05-25 21:08:12 -0300

    cria uma peça, insere no tabuleiro e move ela através de clicks do mouse *-*

--- src/client/game.py
+++ src/client/game.py
@@ -13,6 +13,11 @@ class Window(IntEnum):
     HEIGHT = (Square.HEIGHT + Square.MARGIN) * 8 + Square.MARGIN
 
 
+class Players(IntEnum):
+    P1 = 1
+    P2 = 2
+
+
 class Grid(arcade.ShapeElementList):
     def __init__(self):
         super().__init__()
@@ -53,10 +58,50 @@ class Grid(arcade.ShapeElementList):
                 self.append(current_rect)
 
 
+class Checker(arcade.Sprite):
+    def __init__(self, player, initial_row: int, initial_column: int, is_king: bool = False):
+        self.row = initial_row
+        self.column = initial_column
+        self.player = player
+        self.is_king = is_king
+
+        x, y = self.get_top_left_coord()
+        sprite_filename = ":resources:images/pinball/pool_cue_ball.png"
+        if self.player == Players.P1:
+            sprite_color = arcade.color.RED
+        else:
+            sprite_color = arcade.color.BLUE
+
+        super().__init__(sprite_filename, center_x=x, center_y=y)
+        self.color = sprite_color
+
+    def get_top_left_coord(self):
+        width = Square.WIDTH
+        height = Square.HEIGHT
+        margin = Square.MARGIN
+
+        return (
+            (margin + width) * self.column + margin + width // 2,
+            (margin + height) * self.row + margin + height // 2
+        )
+
+    def move(self, row, column):
+        self.row = row
+        self.column = column
+
+    def update(self):
+        x, y = self.get_top_left_coord()
+        self.center_x = x
+        self.center_y = y
+
+
 class GameWindow(arcade.Window):
     def __init__(self, width: int, height: int, title: str, board):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
+
+        self.checkers_list = None
+        self.checker = None
 
         self.grid = None
         self.board = board
@@ -65,17 +110,25 @@ class GameWindow(arcade.Window):
         self.grid = Grid()
         self.grid.create()
 
-    def on_update(self, delta_time: float):
-        pass
+        self.checker = Checker(Players.P1, 7, 0)
+
+        self.checkers_list = arcade.SpriteList()
+        self.checkers_list.append(self.checker)
 
     def on_draw(self):
         arcade.start_render()
         self.grid.draw()
 
+        self.checkers_list.draw()
+
+    def on_update(self, delta_time: float):
+        self.checkers_list.update()
+
     def on_mouse_press(self, x: float, y: float, button: int, key_modifiers: int):
         column = int(x // (float(Square.WIDTH + Square.MARGIN)))
         row = int(y // (float(Square.HEIGHT + Square.MARGIN)))
 
+        self.checkers_list[0].move(row, column)
         print(column, row)
 
 
